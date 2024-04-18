ee1faeb02b120fcd479ef65568e6f9f93e37809d
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-04-18 19:42:45 +0700

    Add home page

---
+++ components/CustomButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Image from "next/image";
+
+import { CustomButtonProps } from "@/types";
+
+const Button = ({ containerStyles, title, handleClick }: CustomButtonProps) => (
+  <button
+    disabled={false}
+    type={"button"}
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}
+  >
+    <span className={`flex-1`}>{title}</span>
+  </button>
+);
+
+export default Button;
