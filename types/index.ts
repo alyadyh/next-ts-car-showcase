5886f4333e2d0c9ed5ed48c82e5d066dc863c6ed
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-04-19 09:06:15 +0700

    Fix

--- types/index.ts
+++ types/index.ts
@@ -15,16 +15,6 @@ export interface CarProps {
   year: number;
 }
 
-export interface CarCardProps {
-  model: string;
-  make: string;
-  mpg: number;
-  transmission: string;
-  year: number;
-  drive: string;
-  cityMPG: number;
-}
-
 export interface FilterProps {
   manufacturer?: string;
   year?: number;
